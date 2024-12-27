99129abe2635b7b107bac593f623da0f88ec964e
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2024-12-27 13:12:56 -0700

    Add logic so only one decimal can be included Multuple decimals would result in issues when doing calculations, limited user to only adding one decimal per number

--- index.js
+++ index.js
@@ -18,6 +18,27 @@ let numberOne;
 let numberTwo;
 let operator;
 
+const numbers = document.querySelector('.numbers');
+const display = document.querySelector('#display')
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click', () => display.textContent = '');
+
+numbers.addEventListener('click', (e) =>{
+    if(e.target.value == '.' && display.innerText.includes('.')){
+        display.innerText = display.innerText;
+    }
+
+    else if(e.target.value == '.' && !display.innerText.includes('.')){
+        display.innerText = display.innerText + e.target.value;
+    }
+    else{
+        display.innerText = display.innerText + e.target.value;
+    }
+    
+})
+
+
 function operate(numberOne, operator, numberTwo){
     let result;
     switch(operator){
@@ -39,3 +60,5 @@ function operate(numberOne, operator, numberTwo){
     }
     return result;
 }
+
+
